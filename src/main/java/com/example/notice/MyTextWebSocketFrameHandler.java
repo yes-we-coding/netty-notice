0b1926ace3b0d41402a80bfc292f8bc043799d71
etcZYP <972169092@qq.com>
2021-12-06 03:45:34 +0800

    补充

--- src/main/java/com/example/notice/MyTextWebSocketFrameHandler.java
+++ src/main/java/com/example/notice/MyTextWebSocketFrameHandler.java
@@ -85,4 +85,31 @@ public class MyTextWebSocketFrameHandler extends SimpleChannelInboundHandler<Tex
         //一旦有消息，可以主动推送给用户，不需要用户请求服务器获取数据
         messageListenerContainer.addQueueNames(queueName);
     }
+
+    /**
+     * 当客户端连接服务端之后（打开连接）
+     * 获取客户端的channle，并且放到ChannelGroup中去进行管理
+     */
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        users.add(ctx.channel());
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+
+        String channelId = ctx.channel().id().asShortText();
+        System.out.println("客户端被移除，channelId为：" + channelId);
+
+        // 当触发handlerRemoved，ChannelGroup会自动移除对应客户端的channel
+        users.remove(ctx.channel());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        // 发生异常之后关闭连接（关闭channel），随后从ChannelGroup中移除
+        ctx.channel().close();
+        users.remove(ctx.channel());
+    }
 }
